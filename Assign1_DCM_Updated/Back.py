951e13436d338429e3991f1fa57af3cab104f724
IshanP10009 <70909930+IshanP10009@users.noreply.github.com>
2020-10-22 13:25:02 -0400

    Add files via upload

---
+++ Assign1_DCM_Updated/Back.py
@@ -0,0 +1,138 @@
+import mysql.connector
+
+mysql_user = "root" #Insert mysql password name here
+mysql_passwd = "root" #Insert mysql password name here 
+
+db = mysql.connector.connect(
+    host="localhost",
+    user= mysql_user,
+    passwd= mysql_passwd,
+    database = 'application' #this database should be the same as the ap
+)
+mycursor = db.cursor(buffered=True)
+
+
+'''
+Function: New(name,passwrd)
+Paramaeters: name --> input username
+             passwrd --> input password
+
+Description: This function will create a new user if there is enough space in the data base
+             and if input username and password  dont already exist
+'''
+
+def New(name,passwrd):
+
+    mycursor.execute("SELECT COUNT(*) FROM Prac") #count num of rows in one column
+    result = mycursor.fetchone() #will be a tuple with the value (NumOfRows, _______) -> (no second value). so result[0] is the number of rows
+
+    if result[0] == 0: #if no users exist
+        if passwrd == "" or name == "": #empty entry(s)
+            #print("Empty entry")
+            return "empty"
+        else:
+            mycursor.execute("INSERT INTO Prac (username,password,Lower_Rate_Limit,Upper_Rate_Limit,Ventrical_Amplitude,Ventrical_Pulse_Width,Ventrical_Refractory_Period,Attrial_Amplitude,Attrial_Pulse_Width,Attrial_Refractory_Period) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(name, passwrd, 0, 0, 0, 0, 0, 0, 0, 0,))
+            db.commit()
+            return True
+    else:
+        if passwrd == "" or name == "": #empty entry(s)
+            #print("Empty entry")
+            return "empty"
+        else:
+            mycursor.execute("SELECT *FROM Prac ORDER BY personID DESC LIMIT 1 ")
+            #CREATING = True
+            for i in mycursor:
+                #while (CREATING):
+                mycursor.execute("SELECT username,password FROM Prac")
+                # user = True
+                for info in mycursor:
+                    if name == info[0] or passwrd == info[-1]:
+                        user = False
+                        #print("Sorry Username or Password is not available try again!")
+                        return False #since there is an error, no need to continue, so return false.
+                        #user = False
+                    # if (user == True):
+                    #   CREATING = False
+                mycursor.execute("INSERT INTO Prac (username,password,Lower_Rate_Limit,Upper_Rate_Limit,Ventrical_Amplitude,Ventrical_Pulse_Width,Ventrical_Refractory_Period,Attrial_Amplitude,Attrial_Pulse_Width,Attrial_Refractory_Period) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(name, passwrd, 0, 0, 0, 0, 0, 0, 0, 0,))
+                db.commit()
+                #print("It is made!")
+                return True
+            return False
+
+
+'''
+Function CheckUsers() is used to see whether there is enough space in database to create a new user
+'''
+def CheckUsers():
+    mycursor.execute("SELECT COUNT(*) FROM Prac") #count num of rows in one column
+    result = mycursor.fetchone() #will be a tuple with the value (NumOfRows, _______) -> (no second value). so result[0] is the number of rows
+
+    if result[0] == 0: #if no users exist
+        return True #registering should be allowed
+
+    else: #users do exist, so now registering should only be allowed if the max user capacity isn't reached
+        mycursor.execute("SELECT *FROM Prac ORDER BY personID DESC LIMIT 1 ")
+        for i in mycursor:
+            if i[-1] < 10:
+                return True
+        return False
+
+
+''' 
+This will let the user login to their acount if username and password match with the database
+'''
+def Login(name,passwrd):
+    mycursor.execute("SELECT username,password FROM Prac")
+    for info in mycursor:
+        if name == info[0] and passwrd == info[-1]:
+            #print("Login Succesful")
+            #return (name)
+            return 1
+    #print("Incorrect Login information")
+    return 0
+
+
+''' Functions to update the Parameters depending on the user '''
+
+def Change_Lower_Rate_Limit(name,LRL):
+    mycursor.execute("UPDATE Prac SET Lower_Rate_Limit = %s WHERE username = '%s'"%(LRL,name))
+    db.commit()
+
+def Change_Upper_Rate_Limit(name,URL):
+    mycursor.execute("UPDATE Prac SET Upper_Rate_Limit = %s WHERE username = '%s'"%(URL,name))
+    db.commit()
+
+def Change_Ventrical_Amplitude(name,VA):
+    mycursor.execute("UPDATE Prac SET Ventrical_Amplitude = %s WHERE username = '%s'"%(VA,name))
+    db.commit()
+
+def Change_Ventrical_Pulse_Width(name,VPW):
+    mycursor.execute("UPDATE Prac SET Ventrical_Pulse_Width = %s WHERE username = '%s'"%(VPW,name))
+    db.commit()
+
+def Change_Ventrical_Refractory_Period(name,VRP):
+    mycursor.execute("UPDATE Prac SET Ventrical_Refractory_Period = %s WHERE username = '%s'"%(VRP,name))
+    db.commit()
+
+def Change_Attrial_Amplitude(name,AA):
+    mycursor.execute("UPDATE Prac SET Attrial_Amplitude = %s WHERE username = '%s'"%(AA,name))
+    db.commit()
+
+def Change_Attrial_Pulse_Width(name,APW):
+    mycursor.execute("UPDATE Prac SET Attrial_Pulse_Width = %s WHERE username = '%s'"%(APW,name))
+    db.commit()
+
+def Change_Attrial_Refractory_Period(name,ARP):
+    mycursor.execute("UPDATE Prac SET Attrial_Refractory_Period = %s WHERE username = '%s'"%(ARP,name))
+    db.commit()
+##########################################################################################################
+
+
+##### To get any parameter value given username and parameter, will be used to display current stored values
+def Get_Param(name,param):
+    query = "SELECT username,%s FROM Prac WHERE username = '%s'"%(param,name)
+    mycursor.execute(query)
+    for i in mycursor:
+        return (str(i[-1]))
+
+
---
+++ Assign1_DCM_Updated/Creating_Database_for_asg_1.py
@@ -0,0 +1,73 @@
+import mysql.connector
+
+
+mysql_user = "root" #Insert mysql password name here
+mysql_passwd = "root" #Insert mysql password name here 
+
+
+### Steps to create new database and table when first running the code###
+
+### Step 1, uncomment the first set of '''s to run this to create your database ###
+			#Then add the ''' comments again exactly like it was before.
+
+
+'''
+db = mysql.connector.connect(
+    host="localhost",
+    user= mysql_user,
+    passwd= mysql_passwd,
+
+)
+mycursor = db.cursor(buffered=True)
+
+#This database name should be the same as the one in the Back.py file. In this case the name is 'application'
+mycursor.execute("CREATE DATABASE application")
+
+'''
+
+
+
+
+#Step 2: Uncomment '''s here, and KEEP THEM UNCOMMENTED for future mycursor commands.
+
+
+db = mysql.connector.connect(
+    host="localhost",
+    user= mysql_user,
+    passwd= mysql_passwd,
+    database = 'application'
+
+)
+mycursor = db.cursor(buffered=True)
+
+
+
+
+
+#Step 3: Uncomment '''s here and run to create a table. Keep the name as 'Prac'. Then COMMENT IT AGAIN after running.
+'''
+mycursor.execute("CREATE TABLE Prac(username VARCHAR(25), password VARCHAR(25),Lower_Rate_Limit float, Upper_Rate_Limit float, \
+Ventrical_Amplitude float, Ventrical_Pulse_Width float, Ventrical_Refractory_Period float, Attrial_Amplitude float, Attrial_Pulse_Width float,\
+Attrial_Refractory_Period float, personID float PRIMARY KEY AUTO_INCREMENT)")
+'''
+
+
+#Step 4: Make sure Back.py has the same database as the one created here.
+#DONE, Now run the Front.py and everything should be working. 
+
+
+##############################            TESTING PURPOSES HERE           #################################
+
+#Uncomment the line below and run to DELETE THE SELECTED DATABASE, then make it a comment again
+#mycursor.execute("DROP DATABASE [database to delete]")
+
+
+#Uncomment the line below and run to CLEAR THE ENTRIES OF Prac TABLE, then make it a comment again.
+#mycursor.execute("TRUNCATE TABLE Prac")
+
+
+#Uncomment the two lines below and run to ADD A DUMMY USER to the table. Can add multiple dummy users by running it again and again.
+#mycursor.execute("INSERT INTO Prac (username,password,Lower_Rate_Limit,Upper_Rate_Limit,Ventrical_Amplitude,Ventrical_Pulse_Width,Ventrical_Refractory_Period,Attrial_Amplitude,Attrial_Pulse_Width,Attrial_Refractory_Period) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",('First', 'user', 0, 0, 0, 0, 0, 0, 0, 0,))
+#db.commit()
+
+
